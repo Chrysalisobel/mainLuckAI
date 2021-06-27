88223c26ec3abc1a834f7de3af4040ab79216861
Ayisha Rahman <nrayishrahmn@gmail.com>
2021-06-27 08:13:31 +0800

    Test

---
+++ MainLuckAI/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// What happens when the game starts, you'll have these buttons that you can click
+
+public class MenuController : MonoBehaviour
+    {
+
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+    // What happens when you click on "QuitButton"
+
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+
+}
